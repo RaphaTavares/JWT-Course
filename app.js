0b3af812a9c8850b6e41f3ed5afbcdc098a8ec2b
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-08-28 00:58:58 -0300

    Add authRoutes

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { connectionString } = require('./config');
 const app = express();
+const authRoutes = require('./routes/authRoutes');
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const dbURI = connectionString;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
