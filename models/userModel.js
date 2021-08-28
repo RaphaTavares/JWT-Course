281339dca5deb4d4216c99f887b37343ae11b8d5
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-08-28 01:06:56 -0300

    Create user model

---
+++ models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+}, {timestamps: true});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
