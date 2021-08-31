cddfcace848b4f98332121f81a2e75634a8b00bc
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-08-31 19:31:42 -0300

    Implement login handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,10 +59,11 @@ module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
 
     try{
-        const user = await User.create({email, password});
-    } catch(err){
-
+        const user = await User.login(email, password);
+        res.status(200).json({ user: user._id });
+    } 
+    catch (err) {
+        res.status(400).json({});
     }
 
-    res.render('user login');
 }
