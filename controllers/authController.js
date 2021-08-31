cb2b6035ed848dda1eabe3af6f4f6eb011b8f2f1
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-08-31 19:46:32 -0300

    Handle login errors and create JWT on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,16 @@ const handleErrors = err => {
     console.log(err.message, err.code);
     let errors = { email: '', password: ''};
 
+    //login incorrect email
+    if(err.message === 'incorrect email'){
+        errors.email = 'that email is not registered';
+    }
+
+    //login incorrect password
+    if(err.message === 'incorrect password'){
+        errors.email = 'that password is incorrect';
+    }
+
     //duplicate error code
     if(err.code === 11000){
         errors.email = 'Email already registered';
@@ -60,10 +70,15 @@ module.exports.login_post = async (req, res) => {
 
     try{
         const user = await User.login(email, password);
+
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
+
         res.status(200).json({ user: user._id });
     } 
     catch (err) {
-        res.status(400).json({});
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 
 }
