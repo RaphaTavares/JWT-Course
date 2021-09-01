295be418db005c139e0f4fcac2dfe5a8adecf214
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-08-31 23:53:34 -0300

    Add checkUser that check if user is logged middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../config');
+const User = require('../models/userModel');
 
 const requireAuth = (req, res, next) =>{
 
@@ -23,4 +24,27 @@ const requireAuth = (req, res, next) =>{
     }
 }
 
-module.exports = { requireAuth };
+// check current user
+
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if(token){
+        jwt.verify(token, jwtSecret, async (err, decodedToken) =>{
+            if(err){
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            }else{
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    }else{
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
